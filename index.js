14e1bd7975f160571d7dd50a208f55abe2c4557d
shahid33ali <md.shahidlalijafer111@gmail.com>
2024-10-23 11:39:26 +0530

    add health checkpoint

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const app = express();
 
 // Use the Faker API route
 app.use("/api", fakerApiRouter);
+app.get("/health", (req, res) => {
+  try {
+    res.status(200).json({ message: "healthy" });
+  } catch (error) {
+    res.status(400).json({ message: " not healthy" });
+  }
+});
 
 app.listen(5000, () => {
   console.log(`Server is running on port ${PORT}`);
